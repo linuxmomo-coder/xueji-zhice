a7f94596a72234e49a6ed87c9a49c2666dfaa7d6
linuxmomo-coder <linuxmomo@gmail.com>
2026-07-16 17:53:43 +0800

    feat: mount OCR status and confirmation workbench

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalAccountCenter from "./GlobalAccountCenter";
+import GlobalOcrCenter from "./GlobalOcrCenter";
 import GlobalQuestionAdminCenter from "./GlobalQuestionAdminCenter";
 import GlobalRecoveryCenter from "./GlobalRecoveryCenter";
 import "./styles.css";
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
     <GlobalAccountCenter />
     <GlobalRecoveryCenter />
+    <GlobalOcrCenter />
     <GlobalQuestionAdminCenter />
   </React.StrictMode>
 );
