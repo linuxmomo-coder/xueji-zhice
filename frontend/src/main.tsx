bb6a67aa7598ca709debdd97704e02677aae9b41
linuxmomo-coder <linuxmomo@gmail.com>
2026-07-16 17:27:02 +0800

    feat: mount email verification and recovery center

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalAccountCenter from "./GlobalAccountCenter";
+import GlobalRecoveryCenter from "./GlobalRecoveryCenter";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
     <GlobalAccountCenter />
+    <GlobalRecoveryCenter />
   </React.StrictMode>
 );
